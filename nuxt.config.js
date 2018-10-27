536f755af54fa361bf7d92d0a2f082627fef7fb4
biskwikman <djosephhenri@gmail.com>
2018-10-27 17:10:07 +1100

    Changed title from 'my-site' to something more useful

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'my-site',
+    title: 'Maps and other things by Dan Henri',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
